0d510046d0eb8cb25805dc09fdc00f3fb6393398
VadymRyzhuk <ceasarj571@gmail.com>
2023-11-08 18:39:30 +0100

    second

--- src/index.js
+++ src/index.js
@@ -1,15 +1,73 @@
-import axios from 'axios';
-
-axios.defaults.headers.common['x-api-key'] =
-  'live_goF1XJ8NekrIrROIWJxE03bbF4rjAqaHyUoJRlyK6yhL04ajZE5jw15qaKHkW6pj';
-
-function searchCats() {
+const refs = {
+  select: document.querySelector('.breed-select'),
+};
+function fetchBreeds() {
   const BASE_URL = 'https://api.thecatapi.com';
-  const END_POINT = '/v1/breeds/';
-  //const PARAMS = `?hero=${hero}`;
+  const END_POINT = '/v1/breeds';
+
   const url = BASE_URL + END_POINT;
-  console.log(url);
-  return fetch(url, options).then(res => res.json());
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('HTTP error, status = ' + response.status);
+      }
+      return response.json();
+    })
+    .then(breeds => {
+      return breeds;
+    })
+    .catch(error => {
+      console.error('Помилка під час отримання порід: ' + error.message);
+      return [];
+    });
 }
 
-console.log(123);
+fetchBreeds().then(breeds => {
+  //console.log(breeds); // ------------------------------------------------------------DELETE ------------------------------------------
+  breeds.forEach(breed => {
+    const option = document.createElement('option');
+    option.value = breed.id;
+    option.textContent = breed.name;
+    refs.select.appendChild(option);
+    //console.log(breed.reference_image_id);
+    //console.log(breed);
+  });
+});
+
+function fetchCatByBreed(breedId) {
+  const BASE_URL = 'https://api.thecatapi.com';
+  const END_POINT = '/v1/images/search';
+  const PARAMS = `?breed_ids=${breedId}`;
+
+  const url = `${BASE_URL}${END_POINT}${PARAMS}`;
+  //console.log(url); // --------------------------------------------------------------------------------DELETE---------------------------------------
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('HTTP error, status = ' + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // data - це дані про кота
+      return data;
+      //console.log(data);
+    })
+    .catch(error => {
+      console.error(
+        'Помилка під час отримання інформації про кота: ' + error.message
+      );
+      return null;
+    });
+}
+refs.select.addEventListener('change', function () {
+  const selectedBreedId = refs.select.value;
+
+  fetchCatByBreed(selectedBreedId).then(catData => {
+    if (catData) {
+      console.log(catData); // -----------------------------------------INFO --------------------------
+    } else {
+      console.log('Інформація про кота не знайдена.');
+    }
+  });
+});

0d405eebb0d1808ac854c986bd136e8d5b2feb01
VadymRyzhuk <ceasarj571@gmail.com>
2023-11-11 17:08:00 +0100

    90%

---
+++ src/cat-api.js
@@ -0,0 +1,86 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_goF1XJ8NekrIrROIWJxE03bbF4rjAqaHyUoJRlyK6yhL04ajZE5jw15qaKHkW6pj';
+export const refs = {
+  select: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  errorInfo: document.querySelector('.error'),
+};
+function toggleSelectAndLoader(showSelect, showLoader) {
+  refs.select.style.display = showSelect ? 'block' : 'none';
+  toggleLoader(showLoader);
+}
+function toggleLoader(showLoader) {
+  refs.loader.style.display = showLoader ? 'block' : 'none';
+}
+function toggleError(showError, errorMessage) {
+  refs.errorInfo.style.display = showError ? 'block' : 'none';
+
+  // Показувати текст помилки у будь-якому випадку помилки
+  if (showError) {
+    refs.errorInfo.textContent = errorMessage;
+  }
+}
+export function fetchBreeds() {
+  toggleSelectAndLoader(false, true);
+  toggleError(false);
+  const BASE_URL = 'https://api.thecatapi.com';
+  const END_POINT = '/v1/breeds';
+
+  const url = `${BASE_URL}${END_POINT}`;
+  return axios
+    .get(url)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error('HTTP error, status = ' + response.status);
+      }
+      return response.data;
+    })
+    .catch(error => {
+      console.error('Помилка під час отримання порід: ' + error.message);
+      toggleError(true, refs.errorInfo.textContent);
+      return [];
+    })
+    .finally(() => {
+      toggleSelectAndLoader(true, false);
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  toggleSelectAndLoader(true, true);
+  toggleError(false);
+  refs.catInfo.innerHTML = '';
+  const BASE_URL = 'https://api.thecatapi.com';
+  const END_POINT = '/v1/images/search';
+  const PARAMS = `?breed_ids=${breedId}`;
+
+  const url = `${BASE_URL}${END_POINT}${PARAMS}`;
+  //console.log(url); // --------------------------------------------------------------------------------DELETE---------------------------------------
+  return axios
+    .get(url)
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error('HTTP error, status = ' + response.status);
+      }
+      //return response.data;
+      const data = response.data;
+
+      if (data.length === 0) {
+        throw new Error(refs.errorInfo.textContent);
+      }
+
+      return data;
+    })
+    .catch(error => {
+      console.error(
+        'Помилка під час отримання інформації про кота: ' + error.message
+      );
+      toggleError(true);
+      throw error;
+    })
+    .finally(() => {
+      toggleLoader(false); // Приховати завантажувач після отримання інформації про кота
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,45 +1,6 @@
-import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed, refs } from './cat-api.js';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_goF1XJ8NekrIrROIWJxE03bbF4rjAqaHyUoJRlyK6yhL04ajZE5jw15qaKHkW6pj';
-const refs = {
-  select: document.querySelector('.breed-select'),
-  catInfo: document.querySelector('.cat-info'),
-  loader: document.querySelector('.loader'),
-  errorInfo: document.querySelector('.error'),
-};
-
-function toggleLoader(showLoader) {
-  refs.loader.style.display = showLoader ? 'block' : 'none';
-}
-
-function toggleSelectAndLoader(showSelect, showLoader) {
-  refs.select.style.display = showSelect ? 'block' : 'none';
-  toggleLoader(showLoader);
-}
-
-function fetchBreeds() {
-  toggleSelectAndLoader(false, true); //-------------------------------------------------------------------------------------------------------------------------------------------
-  const BASE_URL = 'https://api.thecatapi.com';
-  const END_POINT = '/v1/breeds';
-
-  const url = `${BASE_URL}${END_POINT}`;
-  return axios
-    .get(url)
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error('HTTP error, status = ' + response.status);
-      }
-      return response.data;
-    })
-    .catch(error => {
-      console.error('Помилка під час отримання порід: ' + error.message);
-      return [];
-    })
-    .finally(() => {
-      toggleSelectAndLoader(true, false); // Приховати завантажувач після отримання порід
-    });
-}
+let errorText = ''; //----------------------------------------------------------------------------------------------------------------------
 
 fetchBreeds().then(breeds => {
   //console.log(breeds); // ------------------------------------------------------------DELETE ------------------------------------------
@@ -52,41 +13,6 @@ fetchBreeds().then(breeds => {
     //console.log(breed);
   });
 });
-
-function fetchCatByBreed(breedId) {
-  toggleSelectAndLoader(true, true);
-  refs.catInfo.innerHTML = ''; //-------------------------------------------------------------------------------------------------------------------------------------------
-  const BASE_URL = 'https://api.thecatapi.com';
-  const END_POINT = '/v1/images/search';
-  const PARAMS = `?breed_ids=${breedId}`;
-
-  const url = `${BASE_URL}${END_POINT}${PARAMS}`;
-  //console.log(url); // --------------------------------------------------------------------------------DELETE---------------------------------------
-  return axios
-    .get(url)
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error('HTTP error, status = ' + response.status);
-      }
-      //return response.data;
-      const data = response.data;
-
-      if (data.length === 0) {
-        throw new Error(refs.errorInfo.textContent);
-      }
-
-      return data;
-    })
-    .catch(error => {
-      console.error(
-        'Помилка під час отримання інформації про кота: ' + error.message
-      );
-      throw error;
-    })
-    .finally(() => {
-      toggleLoader(false); // Приховати завантажувач після отримання інформації про кота
-    });
-}
 
 let catName;
 let catDescription;
@@ -111,7 +37,6 @@ refs.select.addEventListener('change', function () {
         });
         refs.errorInfo.classList.add('hidden'); // При успіху, ховаємо блок помилки
       } else {
-        // refs.errorInfo.textContent = 'Кота не знайдено.';
         refs.errorInfo.classList.remove('hidden');
         refs.catInfo.innerHTML = ''; // Очищуємо блок інформації про кота
       }
@@ -128,6 +53,6 @@ function renderCat(catUrl, catName, catDescription, catTemperament) {
      <div class='wrap'> <h1>${catName}</h1>
       <p>${catDescription}</p>
       <p>${catTemperament}</p></div>`;
-  //refs.catInfo.insertAdjacentHTML('afterbegin', markup);
+
   refs.catInfo.innerHTML = markup;
 }

f90f2e820f6f58f35cad809934f7183509a69675
VadymRyzhuk <ceasarj571@gmail.com>
2023-11-12 17:29:27 +0100

    mark commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,55 +1,36 @@
-import axios from 'axios';
+// function toggleSelectAndLoader(showSelect, showLoader) {
+//   refs.select.style.display = showSelect ? 'block' : 'none';
+//   toggleLoader(showLoader);
+// }
+// function toggleLoader(showLoader) {
+//   refs.loader.style.display = showLoader ? 'block' : 'none';
+// }
+// function toggleError(showError, errorMessage) {
+//   refs.errorInfo.style.display = showError ? 'block' : 'none';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_goF1XJ8NekrIrROIWJxE03bbF4rjAqaHyUoJRlyK6yhL04ajZE5jw15qaKHkW6pj';
-export const refs = {
-  select: document.querySelector('.breed-select'),
-  catInfo: document.querySelector('.cat-info'),
-  loader: document.querySelector('.loader'),
-  errorInfo: document.querySelector('.error'),
-};
-function toggleSelectAndLoader(showSelect, showLoader) {
-  refs.select.style.display = showSelect ? 'block' : 'none';
-  toggleLoader(showLoader);
-}
-function toggleLoader(showLoader) {
-  refs.loader.style.display = showLoader ? 'block' : 'none';
-}
-function toggleError(showError, errorMessage) {
-  refs.errorInfo.style.display = showError ? 'block' : 'none';
-
-  if (showError) {
-    refs.errorInfo.textContent = errorMessage;
-  }
-}
+//   if (showError) {
+//     refs.errorInfo.textContent = errorMessage;
+//   }
+// }
 export function fetchBreeds() {
-  toggleSelectAndLoader(false, true);
-  toggleError(false);
   const BASE_URL = 'https://api.thecatapi.com';
   const END_POINT = '/v1/breeds';
 
   const url = `${BASE_URL}${END_POINT}`;
-  return axios
-    .get(url)
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error('HTTP error, status = ' + response.status);
-      }
-      return response.data;
-    })
-    .catch(error => {
-      //console.error('Помилка під час отримання порід: ' + error.message);
-      toggleError(true, refs.errorInfo.textContent);
-      return [];
-    })
-    .finally(() => {
-      toggleSelectAndLoader(true, false);
-    });
+  return axios.get(url).then(response => {
+    return response.data;
+  });
 }
+//     .catch(error => {
+//       toggleError(true, refs.errorInfo.textContent);
+//       return [];
+//     })
+//     .finally(() => {
+//       toggleSelectAndLoader(true, false);
+//     });
+// }
 
 export function fetchCatByBreed(breedId) {
-  toggleSelectAndLoader(true, true);
-  toggleError(false);
   refs.catInfo.innerHTML = '';
   const BASE_URL = 'https://api.thecatapi.com';
   const END_POINT = '/v1/images/search';
@@ -57,26 +38,17 @@ export function fetchCatByBreed(breedId) {
 
   const url = `${BASE_URL}${END_POINT}${PARAMS}`;
 
-  return axios
-    .get(url)
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error('HTTP error, status = ' + response.status);
-      }
+  return axios.get(url).then(response => {
+    const data = response.data;
 
-      const data = response.data;
-
-      if (data.length === 0) {
-        throw new Error(refs.errorInfo.textContent);
-      }
-
-      return data;
-    })
-    .catch(error => {
-      toggleError(true);
-      throw error;
-    })
-    .finally(() => {
-      toggleLoader(false);
-    });
+    return data;
+  });
 }
+//     .catch(error => {
+//       toggleError(true);
+//       throw error;
+//     })
+//     .finally(() => {
+//       toggleLoader(false);
+//     });
+// }
--- src/index.js
+++ src/index.js
@@ -1,55 +1,42 @@
-import { fetchBreeds, fetchCatByBreed, refs } from './cat-api.js';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import axios from 'axios';
 
+axios.defaults.headers.common['x-api-key'] =
+  'live_goF1XJ8NekrIrROIWJxE03bbF4rjAqaHyUoJRlyK6yhL04ajZE5jw15qaKHkW6pj';
+export const refs = {
+  select: document.querySelector('.breed-select'),
+  catInfo: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  errorInfo: document.querySelector('.error'),
+};
 let errorText = '';
 
 fetchBreeds().then(breeds => {
-  breeds.forEach(breed => {
+  const options = breeds.map(breed => {
     const option = document.createElement('option');
     option.value = breed.id;
     option.textContent = breed.name;
-    refs.select.appendChild(option);
+    return option;
   });
+  refs.select.append(...options);
 });
-
-let catName;
-let catDescription;
-let catTemperament;
-let catUrl;
 
 refs.select.addEventListener('change', function () {
   const selectedBreedId = refs.select.value;
 
-  fetchCatByBreed(selectedBreedId)
-    .then(catData => {
-      if (catData) {
-        catData.forEach(data => {
-          catUrl = data.url;
-
-          data.breeds.forEach(info => {
-            catName = info.name;
-            catDescription = info.description;
-            catTemperament = `<span style="font-weight: bold">Temperament:</span> ${info.temperament}`;
-          });
-          renderCat(catUrl, catName, catDescription, catTemperament);
-        });
-        refs.errorInfo.classList.add('hidden');
-      } else {
-        refs.errorInfo.classList.remove('hidden');
-        refs.catInfo.innerHTML = '';
-      }
-    })
-    .catch(error => {
-      refs.errorInfo.textContent = error.message;
-      refs.errorInfo.classList.remove('hidden');
-      refs.catInfo.innerHTML = '';
-    });
+  fetchCatByBreed(selectedBreedId).then(catData => {
+    const catUrl = catData.url;
+    const catName = catData.breeds.name;
+    const catDescription = catData.breeds.description;
+    const catTemperament = catData.breeds.temperament;
+  });
+  const markup = renderCat(catUrl, catName, catDescription, catTemperament);
+  refs.catInfo.innerHTML = markup;
 });
 
 function renderCat(catUrl, catName, catDescription, catTemperament) {
-  const markup = ` <div><img src="${catUrl}" alt="${catName}" width='400px' height='150px'></div>
+  return ` <div><img src="${catUrl}" alt="${catName}" width='400px' height='150px'></div>
      <div class='wrap'> <h1>${catName}</h1>
       <p>${catDescription}</p>
-      <p>${catTemperament}</p></div>`;
-
-  refs.catInfo.innerHTML = markup;
+      <p><span style="font-weight: bold">Temperament:</span>${catTemperament}</p></div>`;
 }
